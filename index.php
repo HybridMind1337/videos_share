8f3f0ca71de214fd9e1ce426f3458ceab758a335
Enriko Todorov <impossiblelimner@gmail.com>
2021-01-13 23:31:36 +0000

    Update index.php

--- index.php
+++ index.php
@@ -2,6 +2,10 @@
 include("./includes/config.php");
 include("./includes/phpBB.php");
 include("./includes/funcs.php");
+if (isset($_POST['del'])) {
+    $id = mysqli_real_escape_string($conn, $_POST['id']);
+    mysqli_query($conn, "DELETE * FROM videos WHERE id = " . $id);
+}
 ?>
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" crossorigin="anonymous">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
@@ -9,7 +13,7 @@ include("./includes/funcs.php");
     <?php
     $getall = mysqli_query($conn, "SELECT * FROM videos WHERE approved = 1");
     while ($row = mysqli_fetch_assoc($getall)) {
-        $getcat = mysqli_query($conn, "SELECT * FROM video_categories WHERE id = ". $row['cat']);
+        $getcat = mysqli_query($conn, "SELECT * FROM video_categories WHERE id = " . $row['cat']);
         $catrow = mysqli_fetch_assoc($getcat);
         $userinfo = userbyid($row['user_id']);
         $embed = explode("watch?v=", $row['url']);
@@ -18,10 +22,15 @@ include("./includes/funcs.php");
         <div class="card">
             <div class="card-header"><?php echo $row['title']; ?></div>
             <div class="card-body">
-                <a href="view.php?id=<?php echo $row['id']; ?>"><img src="<?php echo $imgy; ?>" style="width: 100%" alt="" /></a>
+                <a href="view.php?id=<?php echo $row['id']; ?>"><img src="<?php echo $imgy; ?>" style="width: 100%" alt=""/></a>
+                <?php if ($bb_is_admin) { ?>
+                    <form method="POST">
+                        <input type="hidden" name="id" value="<?php echo $row['id']; ?>">
+                        <button type="submit" name="del" class="m-1 btn btn-danger">Изтрий видеото</button>
+                    </form>
+                <?php } ?>
             </div>
         </div>
 
     <?php } ?>
-
 
