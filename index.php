69570c57cf91da2e72d498a7178e79848ed74130
Enriko Todorov <impossiblelimner@gmail.com>
2021-01-13 23:24:01 +0000

    Update index.php

--- index.php
+++ index.php
@@ -12,16 +12,13 @@ include("./includes/funcs.php");
         $getcat = mysqli_query($conn, "SELECT * FROM video_categories WHERE id = ". $row['cat']);
         $catrow = mysqli_fetch_assoc($getcat);
         $userinfo = userbyid($row['user_id']);
-        $embed = explode("watch?v=", $row['url']); ?>
+        $embed = explode("watch?v=", $row['url']);
+        $imgy = "http://i3.ytimg.com/vi/$embed[1]/maxresdefault.jpg";
+        ?>
         <div class="card">
             <div class="card-header"><?php echo $row['title']; ?></div>
             <div class="card-body">
-                <div class="alert alert-info text-center">
-                    Качен от <b style="color:#<?php echo $userinfo['user_colour']; ?>"><?php echo $userinfo['username']; ?></b>
-                    Добавена на <?php echo date("d.m.Y", $row['date']); ?>
-                    Категория: <?php echo $catrow['name']; ?>
-                </div>
-                <iframe style="width: 100%;height: 500px" src="https://www.youtube.com/embed/<?php echo $embed[1]; ?>"></iframe>
+                <a href="view.php?id=<?php echo $row['id']; ?>"><img src="<?php echo $imgy; ?>" style="width: 100%" alt="" /></a>
             </div>
         </div>
 
